9091b960d8ca98c848c39fd21f925f6a333918d6
Muhammad Kamran <muhammadkamran@MP15-MuhammadK-MI-184.local>
2022-09-06 10:34:58 +0500

    first commit

---
+++ models/lecturer.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  // class Lecturer extends Model {
+  //   /**
+  //    * Helper method for defining associations.
+  //    * This method is not a part of Sequelize lifecycle.
+  //    * The `models/index` file will call this method automatically.
+  //    */
+  //   static associate(models) {
+  //     // define association here
+  //   }
+  // }
+  class Lecturer extends Model {
+    static associate(models) {
+      Lecturer.hasOne(models.Course, {
+        foreignKey: 'lecturer_id',
+        as: 'course',
+      });
+    }
+  };
+  Lecturer.init({
+    lecturer_name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Lecturer',
+  });
+  return Lecturer;
+};
